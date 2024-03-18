16cf1b70648054e8097b3b1212b69c6970c24792
Leyla Gahramanova <77322990+leylagahramanova@users.noreply.github.com>
2024-03-18 15:20:02 +0400

    Update Comments.jsx

--- src/components/mainpage/Comments.jsx
+++ src/components/mainpage/Comments.jsx
@@ -9,7 +9,7 @@ function Comments() {
       <div className="face">
         <img src={woman}/>
       </div>
-      <div className="text">
+      <div className="text1">
         <p>I received the swag package! Thank you so much! We brewed some of the coffee today for everyone here to try. I think it’s great! I worked all weekend (at my second job) so I really needed a kick start this morning!!”</p>
       </div>
       </div>
@@ -17,13 +17,14 @@ function Comments() {
       <div className="face">
       <img src={man}/>
       </div>
-      <div className="text">
+      <div className="text1">
         <p>Your coffee is a big hit with the fellows here. Every morning I make a pot or 2 for any that want some. It is the best tasting coffee I have ever had.</p>
       </div>
       </div>
       </div>
       <div className="more">
           <button><a>VIEW MORE</a></button>
+          {/* Link */}
         </div>
        </div>
   )
